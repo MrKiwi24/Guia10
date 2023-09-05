b8d963741daa24c8b86cfcd15b3da7dad7b1fa94
Sosa Facundo <kiwiprojects.sosafacundon@gmail.com>
2023-09-05 20:46:46 -0300

    Ejercicio 2 GameService and createNewGame added and finished. 05/09 - 20:46

--- ejercicio2/src/Main.java
+++ ejercicio2/src/Main.java
@@ -1,18 +1,10 @@
-import entities.Player;
-import entities.WaterGun;
-import services.PlayerService;
-import services.WaterGunService;
+import services.GameService;
 
 import java.util.ArrayList;
 
 public class Main {
-    public static WaterGunService waterGunService = new WaterGunService();
-    public static PlayerService playerService = new PlayerService();
-    public static ArrayList<WaterGun> loadedGun = waterGunService.loadGun();
-    public static ArrayList<Player> playerList = playerService.loadPlayers();
+    public static GameService gameService = new GameService();
     public static void main(String[] args) {
-
-
-
+        gameService.createNewGame();
     }
 }
